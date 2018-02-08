48f041a3a5deb2bdb8c1cd34d35973bb29cd400e
Marco Nie <nie11kun@outlook.com>
2018-02-08 14:40:36 +0800

    add home button

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 $(function () {
-    var val1 = 0;
     $(".wrapper").on("click", function () {
-        if (val1 == 0) {
-            val1 = 1;
-            $("h2").hide();
-            $(".wrapper").animate({padding: "10em 0em 4em 0em"}, 50);
-            $(".nav").animate({fontSize: "2em"}, 50);
-            $("#box").animate({left: "11%"}, 500);
-            $("footer").css({color: "#222222"});
-        } else {
-            val1 = 0;
-            $("footer").css({color: "#EAE9E5"});
-            $("#box").animate({left: "100%"}, 500);
-            $(".nav").animate({fontSize: "4.4em"}, 50);
-            $(".wrapper").animate({padding: "10em 0em 4em 5em"}, 50);
-            $("h2").show();
-        }
-        
+        $("h2").hide();
+        $("h1").text("MN");
+        $(".wrapper").animate({padding: "3em 0em 4em 0em"}, 50);
+        $(".nav").animate({fontSize: "2em"}, 50);
+        $("#box").animate({left: "10%"}, 500);
+        $("footer").css({color: "#222222"});
+        $("#homeBtn").css({opacity: 1});
     });
+
+    $("#homeBtn").on("click", function () {
+        $("#homeBtn").css({opacity: 0});
+        $("footer").css({color: "#EAE9E5"});
+        $("#box").animate({left: "100%"}, 500);
+        $(".nav").animate({fontSize: "4.4em"}, 50);
+        $(".wrapper").animate({padding: "3em 0em 4em 5em"}, 50);
+        $("h2").show();
+        $("h1").text("Marco Nie");
+    });
+    
 });
